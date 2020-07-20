e6aeaa8183022d7bdd7f4f8ececaad4ff9a3d853
YashBajpai <yash.bajpai_cs.da19@gla.ac.in>
2020-07-20 14:10:23 +0530

    added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//Hello World
+
+public class DemoHelloWorld {
+    public static void main(String[] args){             //Public - access control, void - return type, main - function, string[] is argument and args is its name
+        System.out.println("Hello World!");
+
+  }
+
+}
