9e2d7d517f415dbf021e94a29a7030ccb729db37
YashBajpai <yash.bajpai_cs.da19@gla.ac.in>
2020-07-20 14:27:33 +0530

    Added Files

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo{
+  public static void main(String[] args){
+    int rollNum = 27;
+    int rank = 28;
+    int nig = 222000;
+    System.out.println("Roll Number = " + rollNum);
+    System.out.println();
+    System.out.println("Rank = " + rank);
+    System.out.print("Nig : " + nig);
+  }
+}
